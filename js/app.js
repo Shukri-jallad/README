a866ce7a72429caf0f95a2202c39291028de2bcb
Shukri-jallad <shukri.jallad@yahoo.com>
2021-04-05 09:19:46 +0300

    newproj

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+
+let userDate = prompt('What\'s the date today?');
+console.log (userDate);
+
+let userTime = prompt('what\'s the time now?');
+console.log (userTime);
+
+let userLocation = prompt('Which country do you want to long in from?');
+console.log (userLocation);
+
+alert('The date is: ' + userDate + 'The time is: ' + userTime + 'Your location: ' + userLocation);
